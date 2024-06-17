38020ad3cc872a369169480894739963ec40d162
Edward14 <edward.chang.85y@gmail.com>
2024-06-17 17:02:36 +0800

    edward_test_add_3

--- main.py
+++ main.py
@@ -148,6 +148,33 @@ async def websocket_endpoint(websocket: WebSocket):
 #edward add this#    
 #edward add this#
     
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
+    Uses the Cloud SQL Python Connector package.
+    """
+    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    ip_type = IPTypes.PUBLIC
+
+    connector = Connector()
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    return sqlalchemy.create_engine("postgresql+pg8000://", creator=getconn)
+
+
 pool = connect_with_connector()
 
 class DataEntry(BaseModel):
